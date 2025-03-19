9085097dd9759b54abccdb0bd5fde769f97ecebe
Kaio <kakamorsch@gmail.com>
2025-03-19 18:30:39 -0300

    `Added a section about me`

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <header class="text-center">
-      <h1 class="text-4xl font-bold text-blue-600">Bem-vindo ao meu portfólio!</h1>
-      <p class="mt-4 text-lg">Meu nome é Kaio Morsch e sou desenvolvedor.</p>
-    </header>
-    <section class="mt-8">
-      <h2 class="text-2xl font-semibold">Projetos</h2>
-      <ul class="mt-4">
-        <li><a class="text-blue-500 hover:underline" href="link-do-projeto-1">Projeto 1</a></li>
-        <li><a class="text-blue-500 hover:underline" href="link-do-projeto-2">Projeto 2</a></li>
-        <li><a class="text-blue-500 hover:underline" href="link-do-projeto-3">Projeto 3</a></li>
-      </ul>
-    </section>
-    <footer class="mt-8">
-      <p>Entre em contato: <a class="text-blue-500 hover:underline" href="mailto:kakamorsch@gmail.com">kakamorsch@gmail.com</a></p>
-    </footer>
-  </div>
+	<div
+		class="flex flex-col items-center justify-center min-h-screen bg-gray-100"
+	>
+		<header class="text-center">
+			<h1 class="text-4xl font-bold text-blue-600">
+				Bem-vindo ao meu portfólio!
+			</h1>
+			<p class="mt-4 text-lg">Meu nome é Kaio Morsch e sou desenvolvedor.</p>
+		</header>
+		<div class="flex flex-col items-center justify-center mt-10">
+			<h2 class="text-2xl font-bold text-blue-600">Sobre mim</h2>
+			<p class="mt-4 text-lg">
+				Sou um desenvolvedor apaixonado por resolver problemas, jogos e música.
+				Meu objetivo é criar soluções inovadoras e eficazes para os problemas do
+				mundo real.
+			</p>
+		</div>
+		<footer class="mt-8">
+			<p>
+				Entre em contato:
+				<a
+					class="text-blue-500 hover:underline"
+					href="mailto:kakamorsch@gmail.com"
+					>kakamorsch@gmail.com</a
+				>
+				<a
+					class="text-blue-500 hover:underline"
+					href="https://www.linkedin.com/in/kakamorsch
+         "
+					>Meu LinkedIn</a
+				>
+				<a
+					class="text-blue-500 hover:underline"
+					href="https://x.com/KaioMorsch"
+					>Meu X(Twitter)</a
+				>
+			</p>
+		</footer>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+	name: 'App',
+};
 </script>
-
-<style>
-/* Estilos globais adicionais podem ser adicionados aqui */
-</style>
