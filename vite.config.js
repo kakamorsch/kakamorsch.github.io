cdac7280b090c9a565caf6ba4a216e362aebf80e
Kaio <kakamorsch@gmail.com>
2025-03-19 19:17:03 -0300

    `Update vite.config.js to change base URL to '/kakamorsch.github.io' and remove alias configuration`

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path'
 export default defineConfig({
-  alias:{
-    '@' : path.resolve(__dirname, '.src')
-  },
-  base: '/',
+  base: '/kakamorsch.github.io',
 	plugins: [vue(), tailwindcss()],
 });
