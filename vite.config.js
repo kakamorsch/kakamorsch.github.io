73c215c01ff55523578433aa7bcd4a05b968ee84
Kaio <kakamorsch@gmail.com>
2025-03-19 18:09:58 -0300

    `Update package.json and vite.config.js to modify deploy and base settings`

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path'
 export default defineConfig({
-  base: '/kakamorsch.github.io/',
+  alias:{
+    '@' : path.resolve(__dirname, '.src')
+  },
+  base: '/',
 	plugins: [vue(), tailwindcss()],
 });
